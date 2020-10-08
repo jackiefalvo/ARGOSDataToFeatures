535a868811f2d95274f65317cd1d5677e4b10cb8
jackiefalvo <cristiana.falvo@duke.edu>
2020-10-08 19:43:31 -0400

    (behind the scenes..created a new (cloned) env that works/is what I want I hope) -- updated Spyder to point to that env

--- OpenSpyder.bat
+++ OpenSpyder.bat
@@ -1,4 +1,4 @@
-@set the_env=env859_argos
+@set the_env=env859_ArgosScript
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "%the_env%"
 call "%localappdata%\ESRI\conda\envs\%the_env%\scripts\spyder.exe" -p %cd% 
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
